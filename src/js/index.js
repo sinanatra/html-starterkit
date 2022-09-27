f65e1e413e8f92621c2f2fbc66c610005b60085b
Giacomo Nanni <gn.nanni@gmail.com>
2022-09-27 23:10:33 +0200

    fix: only vanilla

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,9 @@
 import "../style.css";
-import jquery from "jquery";
-
-window.$ = window.jQuery = jquery;
 
 import Website from "./_modules/website";
 import Home from "./_modules/home";
 
-$(function () {
+window.addEventListener('load', function () {
   Website.signature();
   Home.init();
 });
